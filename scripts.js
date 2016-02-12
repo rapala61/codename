4db40bddcaf77899f9ee5e95c067b1112c05e4b0
Rafael Pacas @rapala61 <rapala61@gmail.com>
2016-02-12 16:41:50 -0500

    init commit watz?

---
+++ scripts.js
@@ -0,0 +1,100 @@
+(function( Codename, $ ) {
+  var fields = {};
+
+  Codename.options = {
+    categories: {
+      'address':  ['streetName', 'county'],
+      'commerce': ['product', 'productName'],
+      'hacker':  ['adjective', 'verb'],
+      'finance': ['accountName', 'mask'],
+      'internet': ['domainSuffix'],
+      'name': ['jobArea']
+    }
+  }
+
+  Codename.init = function() {
+    fields = {
+      generatorForm: $('form#codename-generator'),
+      generateBtn: $('[name=generate]'),
+      wdiPrefix: $('[name=wdi-prefix]'),
+      newCodenameElement: $('#new-codename')
+    }
+
+    Codename.UI.init();
+  }
+
+  Codename.generateCodename = function() {
+    var def = $.Deferred();
+
+    $.when(ShakeItspeare.getWords(1, 1), ShakeItspeare.getWords(6, 1))
+      .then(function(poem1, poem2){
+        var prefix = $.trim(fields.wdiPrefix.val()) || "WDI ";
+        var codenameString = prefix + ' ' + poem1 + ' ' + Codename.Util.getRandomString() + '    -     Codename: ' + poem2 + ' ' + Codename.Util.getRandomString();
+        def.resolve(codenameString);
+    })
+
+    ShakeItspeare.getWords(3, 1).then(function( poemWords ) {
+
+    })
+    return def.promise();
+  }
+
+  Codename.Util = {
+    getRandomString: function() {
+      var cat = Codename.Util.getRandomCategory();
+      var sub = Codename.Util.getRandomSubCategory( cat );
+      console.log(sub);
+      return faker[cat][sub]();
+    },
+    getRandomCategory: function() {
+      var keys = Object.keys(Codename.options.categories);
+      var randIdx = Math.floor( Math.random() * keys.length );
+      return keys[randIdx];
+    },
+    getRandomSubCategory: function( category ) {
+      var subCatIdx = Math.floor(Math.random()*Codename.options.categories[category].length);
+      return Codename.options.categories[category][subCatIdx];
+    }
+  }
+
+  Codename.UI = {
+    init: function() {
+      this.bindGenerator();
+    },
+    bindGenerator: function() {
+      fields.generatorForm.on('submit', function(e) {
+        var codename = '';
+        e.preventDefault();
+        Codename.generateCodename().then(function( codename ) {
+          Codename.UI.renderNewCodename( codename );
+        });
+      });
+    },
+    renderNewCodename: function( codename ) {
+      fields.newCodenameElement.text( codename );
+    }
+  }
+}( window.Codename = window.Codename || {}, jQuery));
+
+
+(function( ShakeItspeare, $ ) {
+
+  ShakeItspeare.getWords = function( markov, words ) {
+
+    var markov = markov || 9;
+    var words = words || 1;
+    var def = $.Deferred();
+
+    $.getJSON('http://www.shakeitspeare.com/api/sentence?markov='+markov).done(function( data ) {
+      var sentence = data.sentence.replace('.', '').split(' ');
+      var temp = [];
+
+      for (var i = 0; i < words; i++) {
+        temp.push(sentence[i]);
+      }
+      def.resolve(temp.join(' '));
+    });
+    return def.promise();
+  }
+
+}( window.ShakeItspeare = window.ShakeItspeare || {}, jQuery));
